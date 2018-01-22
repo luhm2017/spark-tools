e49bc5eaee04192619c82a39fea1d98b22c58760
luhuamin <iioveyou123>
2018-01-22 17:45:27 +0800

    creditScore

--- src/main/python/creditCardScore/exec.py
+++ src/main/python/creditCardScore/exec.py
@@ -21,6 +21,12 @@ from logistic_reg import logistic_reg
 from logistic_reg import logit_output
 from CalWOE import _single_woe_trans
 from sklearn.model_selection import train_test_split
+from model_metrics import plot_roc_curve
+from model_metrics import ks_stats
+from model_metrics import lift_lorenz
+from model_metrics import plot_confusion_matrix
+
+
 
 from src.main.python.creditCardScore.CalWOE import _single_woe_trans
 
@@ -47,8 +53,9 @@ y = df['label']
 """
 数据转换
 """
-# df.EDUCATION = df.EDUCATION.map({0: 'unkown', 1:1, 2:2, 3:3, 4:'unkown',5:'unkown', 6: 'unkown'})
-# df.MARRIAGE = df.MARRIAGE.map({0:'unkown', 1:1, 2:0, 3:'unkown'})
+df.EDUCATION = df.EDUCATION.map({0: 'unkown', 1:1, 2:2, 3:3, 4:'unkown',5:'unkown', 6: 'unkown'})
+df.MARRIAGE = df.MARRIAGE.map({0:'unkown', 1:1, 2:0, 3:'unkown'})
+
 
 """
 单个变量woe转换后建立logistic模型
@@ -69,7 +76,7 @@ continnues = ['LIMIT_BAL', 'AGE',
               'PAY_AMT4', 'PAY_AMT5', 'PAY_AMT6',]
 continnues_test = ['AGE']
 # 取所有连续型变量
-dc = df[continnues_test]
+dc = df[continnues]
 
 # 根据选定的方法，对X变量作分箱处理
 def _tempFunc_1(dc, y, method):
@@ -122,8 +129,10 @@ new_df = pd.concat([new_dc,
                     new_PAY_4,
                     new_PAY_5,
                     new_PAY_6], axis=1)
+new_df = pd.concat([new_df, df.SEX, df.EDUCATION, df.MARRIAGE], axis=1)
 
 # WOE transform
+# woe 转换
 df_woe_X, woe_maps, iv_values = woe_trans(new_df.columns, y, new_df)
 
 varNames = df_woe_X.columns
@@ -132,8 +141,6 @@ woe_df = df_woe_X[woe_vars]
 
 # 数据划分
 X_train, X_test, y_train, y_test = train_test_split(woe_df, y, test_size=0.33)
-
-
 
 """
 模型开发
@@ -157,13 +164,13 @@ prob_y_test = logit_result.predict(X_test_metric)
 label_pred_test = pd.np.where(prob_y_test > 0.5, 1, 0)
 
 #ROC 曲线
-# plot_roc_curve(prob_y_test, y_test)
+plot_roc_curve(prob_y_test, y_test)
 # #KS表&KS曲线
-# ks_stattable, _ = ks_stats(prob_y_test, y_test)
+ks_stattable, _ = ks_stats(prob_y_test, y_test)
 # #提升图&lorenz曲线
-# lift_lorenz(prob_y_test, y_test)
+lift_lorenz(prob_y_test, y_test)
 #
-# plot_confusion_matrix(y_test, label_pred_test, labels=[0,1])
+plot_confusion_matrix(y_test, label_pred_test, labels=[0,1])
 
 
 """
