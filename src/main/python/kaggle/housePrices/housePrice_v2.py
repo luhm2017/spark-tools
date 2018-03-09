1290a45f0fffcf9edfb5210536c4d27d75cc4847
luhuamin <iioveyou123>
2018-03-09 18:02:37 +0800

    kaggle模型housePrice_v2

--- src/main/python/kaggle/housePrices/housePrice_v2.py
+++ src/main/python/kaggle/housePrices/housePrice_v2.py
@@ -1,7 +1,10 @@
 import pandas as pd
 import seaborn as sns
 import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
 from scipy.stats import skew
+from scipy.stats.stats import pearsonr
 
 # load data
 train = pd.read_csv("D:/workspace/spark-tools/src/main/data/house-prices/train.csv")
@@ -23,6 +26,7 @@ sns.distplot(np.log1p(train["SalePrice"]))
 
 # 更新目标变量
 train["SalePrice"] = np.log1p(train["SalePrice"])
+
 # 计算偏度、log1p转化
 numeric_feats = all_data.dtypes[all_data.dtypes != "object"].index
 skewed_feats = train[numeric_feats].apply(lambda x:  skew(x.dropna()))
@@ -38,5 +42,43 @@ all_data = all_data.fillna(all_data.mean())
 X_train = all_data[:train.shape[0]]
 X_test = all_data[train.shape[0]:]
 y = train.SalePrice
+
+# 训练模型
+from sklearn.linear_model import Ridge, RidgeCV, ElasticNet, LassoCV, LassoLarsCV
+from sklearn.model_selection import cross_val_score
+
+# 交叉验证模型
+def rmse_cv(model):
+    rmse= np.sqrt(-cross_val_score(model, X_train, y, scoring="neg_mean_squared_error", cv = 5))
+    return(rmse)
+# 岭回归
+model_ridge = Ridge()
+
+alphas = [0.05, 0.1, 0.3, 1, 3, 5, 10, 15, 30, 50, 75]
+cv_ridge = [rmse_cv(Ridge(alpha = alpha)).mean()
+            for alpha in alphas]
+
+cv_ridge = pd.Series(cv_ridge, index = alphas)
+cv_ridge.plot(title = "Validation - Just Do It")
+plt.xlabel("alpha")
+plt.ylabel("rmse")
+cv_ridge.min()
+model_lasso = LassoCV(alphas = [1, 0.1, 0.001, 0.0005]).fit(X_train, y)
+rmse_cv(model_lasso).mean()
+coef = pd.Series(model_lasso.coef_, index = X_train.columns)
+print("Lasso picked " + str(sum(coef != 0)) + " variables and eliminated the other " +  str(sum(coef == 0)) + " variables")
+imp_coef = pd.concat([coef.sort_values().head(10),
+                      coef.sort_values().tail(10)])
+matplotlib.rcParams['figure.figsize'] = (8.0, 10.0)
+imp_coef.plot(kind = "barh")
+plt.title("Coefficients in the Lasso Model")
+
+#let's look at the residuals as well:
+matplotlib.rcParams['figure.figsize'] = (6.0, 6.0)
+
+preds = pd.DataFrame({"preds":model_lasso.predict(X_train), "true":y})
+preds["residuals"] = preds["true"] - preds["preds"]
+preds.plot(x = "preds", y = "residuals",kind = "scatter")
+
 
 print("##############")
